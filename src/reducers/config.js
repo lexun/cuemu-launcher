f85c3fba65a6d06086427df6f5b91257997dcf16
Luke Barbuto <luke.barbuto@gmail.com>
2015-11-08 20:03:49 -0800

    Use consistent whitespace in reducers

--- src/reducers/config.js
+++ src/reducers/config.js
@@ -7,10 +7,13 @@ const initialState = Map({
 
 export default function(state = initialState, action) {
   switch (action.type) {
+
     case actionTypes.CONFIG_LOADED:
       return fromJS(action.options || state)
+
     case actionTypes.CONFIG_UPDATED:
       return state.set(action.field, action.value)
+
     default:
       return state
   }

1d464f3ca9f4ecc4b665cc2a69a469cdb9a8f553
Luke Barbuto <luke.barbuto@gmail.com>
2017-01-27 22:28:59 -1000

    Clean slate for Elm rewrite

--- app/constants/cuemu-addresses.js
+++
@@ -1,1 +0,0 @@
-export const patcherAddress = 'http://patcher1.cuemu.com/patch/'
--- app/containers/app.js
+++
@@ -1,5 +0,0 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./app.production');
-} else {
-  module.exports = require('./app.development');
-}

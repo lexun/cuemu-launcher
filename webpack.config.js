23bb1981111303f5ad634f19bd7f9b11f1e9fc27
Luke Barbuto <luke.barbuto@gmail.com>
2017-01-30 20:24:55 -1000

    Optimize production assets

--- webpack.config.js
+++ webpack.config.js
@@ -9,9 +9,13 @@ const {
 } = require("@webpack-blocks/webpack2");
 
 const devServer = require("@webpack-blocks/dev-server2");
+const extractText = require("@webpack-blocks/extract-text2");
 
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const stylesheetExtractor = new ExtractTextPlugin("index.css");
 
 module.exports = createConfig([
   entryPoint("./src/index"),
@@ -24,26 +28,43 @@ module.exports = createConfig([
       { from: "./src/logo.png", to: "logo.png" },
       { from: "./src/main.js", to: "main.js" }
     ]),
-    new HtmlWebpackPlugin({ inject: true, template: "./src/index.html" })
+    new HtmlWebpackPlugin({ inject: true, template: "./src/index.html" }),
+    new webpack.optimize.UglifyJsPlugin()
   ]),
   loadElm(),
-  env("development", [ devServer() ])
+  env("development", [ devServer(), elmStyleDev() ]),
+  env("production", [
+    elmStyleProd(),
+    addPlugins([ stylesheetExtractor, new webpack.optimize.UglifyJsPlugin() ])
+  ])
 ]);
 
 function loadElm() {
-  return context => ({
-    module: {
-      loaders: [
-        {
-          test: /\.elm$/,
-          exclude: [ /elm-stuff/, /node_modules/, /Stylesheets\.elm$/ ],
-          loader: "elm-hot-loader!elm-webpack-loader"
-        },
-        {
-          test: /Stylesheets\.elm$/,
-          loader: "style-loader!css-loader!elm-css-webpack-loader"
-        }
-      ]
-    }
+  return loader({
+    test: /\.elm$/,
+    exclude: [ /elm-stuff/, /node_modules/, /Stylesheets\.elm$/ ],
+    loader: "elm-hot-loader!elm-webpack-loader"
   });
+}
+
+function elmStyleDev() {
+  return loader({
+    test: /Stylesheets\.elm$/,
+    loader: "style-loader!css-loader!elm-css-webpack-loader"
+  });
+}
+
+function elmStyleProd() {
+  return loader({
+    test: /Stylesheets\.elm$/,
+    loader: stylesheetExtractor.extract({
+      fallbackLoader: "style-loader",
+      loader: "css-loader!elm-css-webpack-loader"
+    }),
+    loaders: undefined
+  });
+}
+
+function loader(object) {
+  return context => ({ module: { loaders: [ object ] } });
 }
